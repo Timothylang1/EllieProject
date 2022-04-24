a415fc3447f9615492f209e5718bbfbe44867af9
Timothylang1 <tlang@macalester.edu>
2022-04-23 19:13:06 -0500

    Checking what I can do with sendTexts

--- sendTexts.py
+++ sendTexts.py
@@ -1,26 +1,19 @@
-import twilio
-# 3126103265
-
-
-# Download the helper library from https://www.twilio.com/docs/python/install
-import os
 from twilio.rest import Client
 
 
-# Find your Account SID and Auth Token at twilio.com/console
-# and set the environment variables. See http://twil.io/secure
+
 account_sid = 'AC1b6b8e92b7158f36445bbed7041b9f0e' # TWILIO SID
 auth_token = '1d98bee4c5dc3406170bea3b4fe16cf4' # TWILIO AUTH TOKEN
 client = Client(account_sid, auth_token)
 
 message = client.messages.create(
-    messaging_service_sid='MG9e2e885ec0b38bda169078f08b404001',
-                              body="I'm watching you",
-                              messaging_service_sid='MG9e2e885ec0b38bda169078f08b404001',
-                              to='+16504474476'
-                          )
+        messaging_service_sid='MG9e2e885ec0b38bda169078f08b404001', # Replace with from_= originating phone number (preferably Ellie's)
+        body="I'm watching you", # Message
+        to='+16504474476' # Receiving phone number
+)
 
 print(message.status)
+print(message.date_created)
 print(message.price)
 print(message.feedback)
 print(message.error_message)
